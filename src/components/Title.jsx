d584a730bd0067887879e9465e0d68b73505334b
Vinitvh <vineet.vh@gmail.com>
2022-09-23 23:11:21 +0530

    Fixed title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,10 +2,7 @@ function Title() {
   return (
     <div className="title">
       <h1>FotoBucket</h1>
-      <h2>Your Pictures</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, sit.
-      </p>
+      <h2>Share Your Pictures.</h2>
     </div>
   );
 }
